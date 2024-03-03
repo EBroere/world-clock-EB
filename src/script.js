cc607b67ac41f3b1ecc0621fb4b572977d01467a
EBroere <e.broere91@gmail.com>
2024-03-03 17:02:21 +0100

    Added the JS

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  //Groningen
+  let groningenElement = document.querySelector("#groningen");
+  let groningenDateElement = groningenElement.querySelector(".date");
+  let groningenTimeElement = groningenElement.querySelector(".time");
+  let groningenTime = moment().tz("Europe/Amsterdam");
 
+  groningenDateElement.innerHTML = groningenTime.format("MMMM Do YYYY");
+  groningenTimeElement.innerHTML = groningenTime.format(
+    "HH:mm:ss:SSS [<small>]A[</small>]"
+  );
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "HH:mm:ss:SSS [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1);
