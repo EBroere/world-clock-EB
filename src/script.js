85860e664a4c8ceaa553b16ea505929e5f482793
EBroere <e.broere91@gmail.com>
2024-03-03 17:50:05 +0100

    Updated JS selector

--- src/script.js
+++ src/script.js
@@ -1,25 +1,50 @@
 function updateTime() {
   //Groningen
   let groningenElement = document.querySelector("#groningen");
-  let groningenDateElement = groningenElement.querySelector(".date");
-  let groningenTimeElement = groningenElement.querySelector(".time");
-  let groningenTime = moment().tz("Europe/Amsterdam");
+  if (groningenElement) {
+    let groningenDateElement = groningenElement.querySelector(".date");
+    let groningenTimeElement = groningenElement.querySelector(".time");
+    let groningenTime = moment().tz("Europe/Amsterdam");
 
-  groningenDateElement.innerHTML = groningenTime.format("MMMM Do YYYY");
-  groningenTimeElement.innerHTML = groningenTime.format(
-    "HH:mm:ss:SSS [<small>]A[</small>]"
-  );
+    groningenDateElement.innerHTML = groningenTime.format("MMMM Do YYYY");
+    groningenTimeElement.innerHTML = groningenTime.format(
+      "HH:mm:ss:SSS [<small>]A[</small>]"
+    );
+  }
 
   //Paris
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-
-  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format(
-    "HH:mm:ss:SSS [<small>]A[</small>]"
-  );
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "HH:mm:ss:SSS [<small>]A[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city"> 
+  <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format(
+      "HH:mm:ss:SSS"
+    )} <small>${cityTime.format("A")}</small></div>
+    </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
