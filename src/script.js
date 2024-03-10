a9b02387011193e0c8394bfb39a21e45ee54db6a
EBroere <e.broere91@gmail.com>
2024-03-10 13:00:45 +0100

    implemented analoge clock

--- src/script.js
+++ src/script.js
@@ -1,3 +1,39 @@
+// Analog clock
+
+setInterval(setClocks, 1000);
+
+const timezones = ["Europe/Amsterdam"];
+
+function setClocks() {
+  timezones.forEach((timezone, index) => {
+    const clock = document.querySelectorAll(".clock")[index];
+    setClock(clock, timezone);
+  });
+}
+
+function setClock(clock, timezone) {
+  const hourHand = clock.querySelector("[data-hour-hand]");
+  const minuteHand = clock.querySelector("[data-minute-hand]");
+  const secondHand = clock.querySelector("[data-second-hand]");
+
+  const currentDate = moment().tz(timezone);
+
+  const secondsRatio = currentDate.seconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.minutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.hours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClocks();
+
+// Digital clock
+
 function updateTime() {
   //Groningen
   let groningenElement = document.querySelector("#groningen");
@@ -8,23 +44,12 @@ function updateTime() {
 
     groningenDateElement.innerHTML = groningenTime.format("MMMM Do YYYY");
     groningenTimeElement.innerHTML = groningenTime.format(
-      "HH:mm:ss:SSS [<small>]A[</small>]"
-    );
-  }
-
-  //Paris
-  let parisElement = document.querySelector("#paris");
-  if (parisElement) {
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format(
-      "HH:mm:ss:SSS [<small>]A[</small>]"
+      "HH:mm:ss [<small>]A[</small>]"
     );
   }
 }
 
+//Timezone digital clock
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -39,11 +64,10 @@ function updateCity(event) {
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format(
-      "HH:mm:ss:SSS"
-    )} <small>${cityTime.format("A")}</small></div>
+    <div class="time">${cityTime.format("HH:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
     </div>
-    <a href="/">All cities</a>
   `;
 }
 
